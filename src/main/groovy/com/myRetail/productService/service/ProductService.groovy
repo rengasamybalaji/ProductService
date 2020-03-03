d27c7f59298697a91aa538b5a07c9f06c59c8d5f
Balaji Rengasamy <balaji.rengasamy@bestbuy.com>
2020-03-02 20:45:29 -0600

    Implemented completableFuture

--- src/main/groovy/com/myRetail/productService/service/ProductService.groovy
+++ src/main/groovy/com/myRetail/productService/service/ProductService.groovy
@@ -1,13 +1,15 @@
 package com.myRetail.productService.service
 
-import com.myRetail.productService.dto.ProductPriceEntity
-import com.myRetail.productService.dto.Product
-import com.myRetail.productService.dto.RedskyProduct
 import com.myRetail.productService.client.ProductClient
+import com.myRetail.productService.dto.Product
+import com.myRetail.productService.dto.ProductPriceEntity
+import com.myRetail.productService.dto.RedskyProduct
 import com.myRetail.productService.repository.ProductPriceRepository
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+
+import java.util.concurrent.CompletableFuture
 
 @Service
 @Slf4j
@@ -25,20 +27,28 @@ class ProductService {
   Product getProduct(long productId) {
 
     long now = System.currentTimeMillis()
-    RedskyProduct redskyProduct = productClient.fetchProduct(productId)
+    CompletableFuture<RedskyProduct> redSkyFuture = CompletableFuture.supplyAsync {
+      return productClient.fetchProduct(productId)
+      }
 
-    Optional<ProductPriceEntity> optionalProductPriceEntity = productPriceRepository.findById(productId)
-    ProductPriceEntity productPriceEntity = optionalProductPriceEntity.orElse(null)
+    CompletableFuture<ProductPriceEntity> dbFuture = CompletableFuture.supplyAsync {
+      Optional<ProductPriceEntity> optionalProductPriceEntity = productPriceRepository.findById(productId)
+      return optionalProductPriceEntity.orElse(null)
+      }
 
-    Product product = new Product(productId: productId,
-      productName: redskyProduct?.product?.item?.productDesc?.title,
-      currentPrice: productPriceEntity ? new Product.Price(value:  productPriceEntity?.price?.value,
+    CompletableFuture<Product> combinedFuture = redSkyFuture.thenCombine(dbFuture)
+      {
+        redskyProduct, productPriceEntity -> Product product = new Product(productId: productId,
+        productName: redskyProduct?.product?.item?.productDesc?.title,
+        currentPrice: productPriceEntity ? new Product.Price(value: productPriceEntity?.price?.value,
         currencyCode: productPriceEntity?.price?.currencyCode) : null)
+      return product
+      }
 
     long timeTakenToFetchProduct = System.currentTimeMillis() - now
     metricService.recordTimer("getProduct", timeTakenToFetchProduct)
 
-    return product
+    return combinedFuture.get()
   }
 
   void updateProductPrice(Product product) {
